43911eb17857431937aa493a2779d1d6fc4450cf
Pratham <crce.9383.aids@gmail.com>
2023-09-24 21:21:03 +0530

    Initial Commit

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import logo from "../../images/logo.png";
+
+const Footer = () => (
+  <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
+    
+  </div>
+);
+
+export default Footer;
